73e04546d22181d14548a6f3522e4ce94fd56efd
deadln <glebuch2000@list.ru>
2019-09-12 16:09:34 +0300

    Little issue

--- Практика/Practice2/src/com/company/TestAuthor.java
+++ Практика/Practice2/src/com/company/TestAuthor.java
@@ -1,12 +1,20 @@
 package com.company;
 import java.lang.*;
+import	java.util.Scanner;
 
 
 public class TestAuthor {
 
     public static void runTest()
     {
-        Author gh = new Author("Herbert Schildt", "h.schildt@gmail.com", true);
+        String name, email;
+        boolean male;
+        Scanner in = new Scanner(System.in);
+        name = in.nextLine();
+        email = in.nextLine();
+        male = in.nextBoolean();
+
+        Author gh = new Author(name, email, male);
         System.out.println(gh);
         gh.setEmail("luke12skywalker@gmail.com");
         System.out.println(gh);
